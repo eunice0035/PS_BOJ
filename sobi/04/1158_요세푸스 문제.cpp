291b8f9a7a37afdb72def492ef2b00b9f14e3bca
Jeong Sobi <j508193@gmail.com>
2021-02-21 19:30:38 +0900

    210221

--- sobi/04/1158_요세푸스 문제.cpp
+++ sobi/04/1158_요세푸스 문제.cpp
@@ -1,51 +1,36 @@
 #include<iostream>
 #include<list>
-#include <ctime>
 using namespace std;
 
+
 int main(void) {
-	clock_t start1, start2, end1, end2;
-	float res1, res2;
-	start1 = clock();
+   list<int> josephus;
+   int n, k;
+   cin >> n >> k;
+   for (int i = 1; i < n + 1; i++) {
+      josephus.push_back(i);
+   }
+   list<int>::iterator t;
+   t = josephus.begin();
 
-	list<int> josephus;
-	int n, k;
-	cin >> n >> k;
-	for (int i = 1; i < n + 1; i++) {
-		josephus.push_back(i);
-	}
-	list<int>::iterator t;
-	t = josephus.begin();
-	int count = k - 1;
-	cout << "<";
-	int total = 0;
-	while (josephus.size() != 0) {
-		total++;
-		t++;
-		count--;
-		if (count == 0) {
-			if (t == josephus.end()) {
-				t = josephus.begin();
-			}
-			cout << *t;
-			t = josephus.erase(t);
-			if (josephus.size() == 0) {
-				cout << ">";
-				break;
-			}
+   cout << "<";
+   while (josephus.size() != 0) {
+      for (int i = 0; i < k-1; i++) {
 
-			else
-				cout << ", ";
-			count = k - 1;
-		}
-		if (t == josephus.end()) {
-			t = josephus.begin();
-		}
-	}
-	cout << total;
-	end1 = clock();
-	res1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
-	cout << "excute time : " << res1 << '\n';
-
-	return 0;
+         t++;
+         if (t == josephus.end()) {
+            t = josephus.begin();
+         }
+      }
+      cout << *t;
+      t = josephus.erase(t);
+      if (josephus.size() == 0)
+         cout << ">";
+      else
+         cout << ", ";
+      if (t == josephus.end()) {
+         t = josephus.begin();
+      }
+   }
+   return 0;
 }
