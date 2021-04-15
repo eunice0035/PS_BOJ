4553a4db673537d0075fe752bb145aec44123c29
eunice0035 <eunice7357@gmail.com>
2021-04-15 22:07:26 +0900

    21.04.15 add

--- eunseong/L09/2178_미로탐색.cpp
+++ eunseong/L09/2178_미로탐색.cpp
@@ -2,43 +2,37 @@
 using namespace std;
 
 int n, m;
-int board[101][101];
+char board[102][102]={'0',};
+//(0,0)에서부터 이동거리 계산
+int dis[102][102]={0,};
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
+int main(void){
+    //input
+    cin >>n >> m;
+    for(int i=0; i<n; i++){
+        string temp;
+        cin >> temp;
+        for(int j=0; j<m; j++)
+            board[i][j] = temp[j];
+    }
 
-int bfs(int start_x, int start_y){
-    queue<pair<int, int>> q;
-    q.push({start_x, start_y});
+    queue<pair<int,int>> Q;
+    dis[0][0] = 1;
+    Q.push({0,0});
 
-    while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
+    while(!Q.empty()){
+        pair<int,int> cur = Q.front();
+        Q.pop();
         for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(board[nx][ny] == 0) continue;
-            if(board[nx][ny] == 1){
-                board[nx][ny] = board[x][y] + 1;
-                q.push({nx, ny});
-            }
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
+            if(nx < 0 || nx>=n || ny <0 || ny >=m) continue;
+            if(board[nx][ny] == '0' || dis[nx][ny]> 0) continue;
+            dis[nx][ny] = dis[cur.first][cur.second] + 1;
+            Q.push({nx,ny});
         }
     }
-    return board[n-1][m-1];
-}
-
-int main(){
-	cin>>n>>m;
-	for(int i=0; i<n; i++){
-		for(int j=0; j<m; j++){
-			scanf("%1d", &board[i][j]);
-            }
-		}
-	
-	cout<<bfs(0,0)<<endl;
-
+    cout << dis[n-1][m-1];
 }
--- eunseong/L09/7576_토마토.cpp
+++ eunseong/L09/7576_토마토.cpp
@@ -1,66 +1,49 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+int n,m;
+int toma[1002][1002];
+int dist[1002][1002];
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
 int main() {
+    cin >> m >> n;
+    queue<pair<int,int>> Q;
 
-    int dx[4] = { 1, 0, -1, 0 };
-    int dy[4] = { 0, 1, 0, -1 };
-    bool traverse;
-    int m, n, i, j, k, seg = 0, cnt = 0, ans = 0;
-    queue<pair<int, int> > q;
-
-    scanf("%d %d", &m, &n);
-
-    int arr[n+1][m+1];
-    bool v[n+1][m+1];
-    
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= m; j++) {
-            v[i][j] = false;
-            scanf("%d", &arr[i][j]);
-            if (arr[i][j] == 1) {
-                q.push(make_pair(i, j));
-                seg++;
-            }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> toma[i][j];
+            if(toma[i][j]==1) Q.push({i,j});
+            if(toma[i][j]==0) dist[i][j] = -1;
         }
     }
 
-    while(!q.empty()) {
-        cnt = 0;
-        traverse = false;
-        for (i = 0; i < seg; i++) {
-
-            pair<int, int> newc = q.front();
-            q.pop();
-
-            v[newc.first][newc.second] = true;
-
-            for (k = 0; k < 4; k++) {
-                int newy = newc.first + dy[k];
-                int newx = newc.second + dx[k];
-
-                if (newx > 0 && newx <= m && newy > 0 && newy <= n && !v[newy][newx] && arr[newy][newx] == 0) {
-                    traverse = true;
-                    arr[newy][newx] = 1;
-                    q.push(make_pair(newy, newx));
-                    cnt++;
-                }
-            }
+    while(!Q.empty()){
+        pair<int,int> cur = Q.front();
+        Q.pop();
+        for(int i=0; i<4; i++){
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
+            
+            if(nx<0 || nx>=n || ny <0 || ny>=m) continue;
+            if(dist[nx][ny] >= 0) continue;
+            toma[nx][ny] = 1;
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
+            Q.push({nx,ny});
         }
-        seg = cnt;
-        if (traverse) ans++;
     }
 
-    bool check = true;
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= m; j++) {
-            if (!v[i][j] && arr[i][j] == 0) {
-                printf("-1\n");
+    int day = 0;
+    for(int i=0; i<n; i++){
+        for(int j =0; j<m; j++){
+            if(dist[i][j] == -1){
+                cout << -1;
                 return 0;
             }
+            day = max(day,dist[i][j]);
         }
     }
-
-    printf("%d\n", ans);
-    return 0;
+    cout << day;
 }
